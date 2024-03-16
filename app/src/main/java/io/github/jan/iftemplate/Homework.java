fb8f004cac63e51285fa258269c668b2eb28ccb4
Jan <jan.m.tennert@gmail.com>
2024-03-16 13:46:46 +0100

    remove homework

--- app/src/main/java/io/github/jan/iftemplate/Homework.java
+++ app/src/main/java/io/github/jan/iftemplate/Homework.java
@@ -1,35 +1,24 @@
 package io.github.jan.iftemplate;
 
 /**
- * Aufgabe 1:
  * Fülle die Klasse Homework mit den notwendigen Attributen und Methoden.
  */
 public class Homework {
 
-    private final String content;
-    private final String dueTime;
-    private boolean done;
-
-    public Homework(String content, String dueTime, boolean done) {
-        this.content = content;
-        this.dueTime = dueTime;
-        this.done = done;
-    }
-
     public String getContent() {
-        return content;
+        return "";
     }
 
     public String getDueTime() {
-        return dueTime;
+        return "";
     }
 
     public boolean isDone() {
-        return done;
+        return false;
     }
 
     public void setDone(boolean done) {
-        this.done = done;
+
     }
 
 }
